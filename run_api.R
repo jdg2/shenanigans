cbc6e71ff0e9582cdde821fc6212224d548a3117
Jake Gundrum <jake.gundrum@breakthroughfuel.com>
2019-04-12 15:08:26 -0500

    IT WORKS FINALLY

--- run_api.R
+++ run_api.R
@@ -1,7 +1,7 @@
 library(plumber)
 
-host_ip <- system("curl ifconfig.co", intern = T)
+# host_ip <- system("curl ifconfig.co", intern = T)
 
 rs <- plumber::plumb("./actual_api.R")
-rs$run(port = 9999, host_ip )
+rs$run(port = 9999, "0.0.0.0" )
 
