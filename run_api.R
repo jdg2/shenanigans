0511d017693bfaf18352bd45c44586f638cf1c21
Jake Gundrum <jake.gundrum@breakthroughfuel.com>
2019-04-12 14:53:49 -0500

    message

--- run_api.R
+++ run_api.R
@@ -1,5 +1,7 @@
 library(plumber)
 
+host_ip <- system("curl ifconfig.co", intern = T)
+
 rs <- plumber::plumb("./actual_api.R")
-rs$run(port = 9999, host = system("curl ifconfig.co", intern = T) )
+rs$run(port = 9999, host_ip )
 
